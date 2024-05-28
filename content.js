cab9e504758b9b573e8ff919b8b5ce2d74d20846
gmelon <hsh1769@naver.com>
2024-05-28 18:08:40 +0900

    chore: 포맷팅

--- content.js
+++ content.js
@@ -1,13 +1,16 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if(request.message === 'getHtml') {
-        let inputs = [];
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.message === "getHtml") {
+    let inputs = [];
 
-        for(checkbox of document.getElementsByTagName("div")) {
-            if (checkbox.classList.contains("notion-to_do-block") && checkbox.innerText.trim().length > 0) {
-                inputs.push(checkbox.innerText);
-            }
-        }
-
-        sendResponse({inputs: inputs});
+    for (checkbox of document.getElementsByTagName("div")) {
+      if (
+        checkbox.classList.contains("notion-to_do-block") &&
+        checkbox.innerText.trim().length > 0
+      ) {
+        inputs.push(checkbox.innerText);
+      }
     }
+
+    sendResponse({ inputs: inputs });
+  }
 });
